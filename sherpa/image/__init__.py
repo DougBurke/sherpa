6cd317275b086694ff8928e84a9ce6a04d175e1f
Douglas Burke <dburke.gw@gmail.com>
2025-11-06 10:01:12 -0500

    image: add basic typing support
    Not everything is typed, in particular the data argument for the
    xpaset calls. The backends are also not typed.
    
    There are also some code "refresh" changes - e.g. use of super()
    rather than explicitly naming the superclass, and using the
    staticmethod decorator rather than as a function, since that seems to
    make pyright happier.

--- sherpa/image/__init__.py
+++ sherpa/image/__init__.py
@@ -33,13 +33,16 @@ References
 """
 
 import logging
+from typing import cast
 
 import numpy as np
 
+from sherpa.data import Data2D
+from sherpa.models.model import Model
 from sherpa.utils import NoNewAttributesAfterInit, bool_cast
 
+
 warning = logging.getLogger(__name__).warning
-backend = None
 
 try:
     from . import ds9_backend as backend
@@ -47,9 +50,10 @@ try:
 except Exception as e:
     # if DS9 is not found for some reason, like inside gdb
     # give a useful warning and fall back on dummy_backend of noops
-    warning("imaging routines will not be available, \n" +
-            "failed to import sherpa.image.ds9_backend due to \n'%s: %s'" %
-            (type(e).__name__, str(e)))
+    warning("imaging routines will not be available, \n"
+            "failed to import sherpa.image.ds9_backend due to \n"
+            "'%s: %s'",
+            type(e).__name__, str(e))
     from . import dummy_backend as backend
 
 
@@ -61,20 +65,18 @@ __all__ = ('Image', 'DataImage', 'ModelImage', 'RatioImage',
 class Image(NoNewAttributesAfterInit):
     """Base class for sending image data to an external viewer."""
 
-    def __init__(self):
-        NoNewAttributesAfterInit.__init__(self)
-
-    def close():
+    @staticmethod
+    def close() -> None:
         """Stop the image viewer."""
         backend.close()
-    close = staticmethod(close)
 
-    def delete_frames():
+    @staticmethod
+    def delete_frames() -> None:
         """Delete all the frames open in the image viewer."""
         backend.delete_frames()
-    delete_frames = staticmethod(delete_frames)
 
-    def get_region(coord):
+    @staticmethod
+    def get_region(coord: str) -> str:
         """Return the region defined in the image viewer.
 
         Parameters
@@ -90,9 +92,13 @@ class Image(NoNewAttributesAfterInit):
 
         """
         return backend.get_region(coord)
-    get_region = staticmethod(get_region)
 
-    def image(self, array, shape=None, newframe=False, tile=False):
+    def image(self,
+              array: np.ndarray,
+              shape: tuple[int, ...] | None = None,
+              newframe: bool = False,
+              tile: bool = False
+              ) -> None:
         newframe = bool_cast(newframe)
         tile = bool_cast(tile)
         if shape is None:
@@ -100,15 +106,16 @@ class Image(NoNewAttributesAfterInit):
         else:
             backend.image(array.reshape(shape), newframe, tile)
 
-    def open():
+    @staticmethod
+    def open() -> None:
         """Start the image viewer."""
         backend.open()
-    open = staticmethod(open)
 
-    def set_wcs(self, keys):
+    def set_wcs(self, keys) -> None:
         backend.wcs(keys)
 
-    def set_region(reg, coord):
+    @staticmethod
+    def set_region(reg: str, coord: str) -> None:
         """Set the region to display in the image viewer.
 
         Parameters
@@ -121,9 +128,9 @@ class Image(NoNewAttributesAfterInit):
 
         """
         backend.set_region(reg, coord)
-    set_region = staticmethod(set_region)
 
-    def xpaget(arg):
+    @staticmethod
+    def xpaget(arg: str) -> str:
         """Return the result of an XPA call to the image viewer.
 
         Send a query to the image viewer.
@@ -139,9 +146,9 @@ class Image(NoNewAttributesAfterInit):
 
         """
         return backend.xpaget(arg)
-    xpaget = staticmethod(xpaget)
 
-    def xpaset(arg, data=None):
+    @staticmethod
+    def xpaset(arg: str, data=None) -> str:
         """Return the result of an XPA call to the image viewer.
 
         Send a command to the image viewer.
@@ -155,7 +162,6 @@ class Image(NoNewAttributesAfterInit):
 
         """
         return backend.xpaset(arg, data=None)
-    xpaset = staticmethod(xpaset)
 
 
 class DataImage(Image):
@@ -173,14 +179,14 @@ class DataImage(Image):
 
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.y = None
         self.eqpos = None
         self.sky = None
         self.name = 'Data'
-        Image.__init__(self)
+        super().__init__()
 
-    def __str__(self):
+    def __str__(self) -> str:
         y = self.y
         if self.y is not None:
             y = np.array2string(self.y, separator=',', precision=4,
@@ -190,7 +196,7 @@ class DataImage(Image):
                 ('eqpos  = %s\n' % self.eqpos) +
                 ('sky    = %s\n' % self.sky))
 
-    def prepare_image(self, data):
+    def prepare_image(self, data: Data2D) -> None:
         self.y = data.get_img()
         self.eqpos = getattr(data, 'eqpos', None)
         self.sky = getattr(data, 'sky', None)
@@ -200,21 +206,25 @@ class DataImage(Image):
             if obj is not None:
                 self.name = str(obj).replace(" ", "_")
 
-    def image(self, shape=None, newframe=False, tile=False):
+    def image(self,
+              shape: tuple[int, ...] | None = None,
+              newframe: bool = False,
+              tile: bool = False
+              ) -> None:
         Image.image(self, self.y, shape, newframe, tile)
         Image.set_wcs(self, (self.eqpos, self.sky, self.name))
 
 
 class ModelImage(Image):
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.name = 'Model'
         self.y = None
         self.eqpos = None
         self.sky = None
-        Image.__init__(self)
+        super().__init__()
 
-    def __str__(self):
+    def __str__(self) -> str:
         y = self.y
         if self.y is not None:
             y = np.array2string(self.y, separator=',', precision=4,
@@ -224,29 +234,33 @@ class ModelImage(Image):
                 ('eqpos  = %s\n' % self.eqpos) +
                 ('sky    = %s\n' % self.sky))
 
-    def prepare_image(self, data, model):
+    def prepare_image(self, data: Data2D, model: Model) -> None:
         self.y = data.get_img(model)
         self.y = self.y[1]
         self.eqpos = getattr(data, 'eqpos', None)
         self.sky = getattr(data, 'sky', None)
 
-    def image(self, shape=None, newframe=False, tile=False):
+    def image(self,
+              shape: tuple[int, ...] | None = None,
+              newframe: bool = False,
+              tile: bool = False
+              ) -> None:
         Image.image(self, self.y, shape, newframe, tile)
         Image.set_wcs(self, (self.eqpos, self.sky, self.name))
 
 
 class SourceImage(ModelImage):
-    def __init__(self):
-        ModelImage.__init__(self)
+    def __init__(self) -> None:
+        super().__init__()
         self.name = 'Source'
 
-    def prepare_image(self, data, model):
+    def prepare_image(self, data: Data2D, model: Model) -> None:
         # self.y = data.get_img(model)
         # self.y = self.y[1]
 
         self.y = data.eval_model(model)
         data._check_shape()
-        self.y = self.y.reshape(*data.shape)
+        self.y = self.y.reshape(*cast(tuple[int, ...], data.shape))
 
         self.eqpos = getattr(data, 'eqpos', None)
         self.sky = getattr(data, 'sky', None)
@@ -254,14 +268,14 @@ class SourceImage(ModelImage):
 
 class RatioImage(Image):
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.name = 'Ratio'
         self.y = None
         self.eqpos = None
         self.sky = None
-        Image.__init__(self)
+        super().__init__()
 
-    def __str__(self):
+    def __str__(self) -> str:
         y = self.y
         if self.y is not None:
             y = np.array2string(self.y, separator=',', precision=4,
@@ -279,27 +293,31 @@ class RatioImage(Image):
         model[bad] = 1.0
         return (data / model)
 
-    def prepare_image(self, data, model):
+    def prepare_image(self, data: Data2D, model: Model) -> None:
         self.y = data.get_img(model)
         self.y = self._calc_ratio(self.y)
         self.eqpos = getattr(data, 'eqpos', None)
         self.sky = getattr(data, 'sky', None)
 
-    def image(self, shape=None, newframe=False, tile=False):
+    def image(self,
+              shape: tuple[int, ...] | None = None,
+              newframe: bool = False,
+              tile: bool = False
+              ) -> None:
         Image.image(self, self.y, shape, newframe, tile)
         Image.set_wcs(self, (self.eqpos, self.sky, self.name))
 
 
 class ResidImage(Image):
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.name = 'Residual'
         self.y = None
         self.eqpos = None
         self.sky = None
-        Image.__init__(self)
+        super().__init__()
 
-    def __str__(self):
+    def __str__(self) -> str:
         y = self.y
         if self.y is not None:
             y = np.array2string(self.y, separator=',', precision=4,
@@ -312,44 +330,48 @@ class ResidImage(Image):
     def _calc_resid(self, ylist):
         return ylist[0] - ylist[1]
 
-    def prepare_image(self, data, model):
+    def prepare_image(self, data: Data2D, model: Model) -> None:
         self.y = data.get_img(model)
         self.y = self._calc_resid(self.y)
         self.eqpos = getattr(data, 'eqpos', None)
         self.sky = getattr(data, 'sky', None)
 
-    def image(self, shape=None, newframe=False, tile=False):
+    def image(self,
+              shape: tuple[int, ...] | None = None,
+              newframe: bool = False,
+              tile: bool = False
+              ) -> None:
         Image.image(self, self.y, shape, newframe, tile)
         Image.set_wcs(self, (self.eqpos, self.sky, self.name))
 
 
 class PSFImage(DataImage):
 
-    def prepare_image(self, psf, data=None):
+    def prepare_image(self, psf, data=None) -> None:
         psfdata = psf.get_kernel(data, False)
-        DataImage.prepare_image(self, psfdata)
+        super().prepare_image(psfdata)
         self.name = psf.kernel.name
 
 
 class PSFKernelImage(DataImage):
 
-    def prepare_image(self, psf, data=None):
+    def prepare_image(self, psf, data=None) -> None:
         psfdata = psf.get_kernel(data)
-        DataImage.prepare_image(self, psfdata)
+        super().prepare_image(psfdata)
         self.name = 'PSF_Kernel'
 
 
 class ComponentSourceImage(ModelImage):
 
-    def prepare_image(self, data, model):
-        ModelImage.prepare_image(self, data, model)
+    def prepare_image(self, data: Data2D, model: Model) -> None:
+        super().prepare_image(data, model)
         # self.name = "Source component '%s'" % model.name
         self.name = "Source_component"
 
 
 class ComponentModelImage(ModelImage):
 
-    def prepare_image(self, data, model):
-        ModelImage.prepare_image(self, data, model)
+    def prepare_image(self, data: Data2D, model: Model) -> None:
+        super().prepare_image(data, model)
         # self.name = "Model component '%s'" % model.name
         self.name = "Model_component"
